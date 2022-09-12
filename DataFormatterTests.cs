b9697a0f78a8235a0dce9f729f8f7fb7ab897d3f
panchei19 <110833799+panchei19@users.noreply.github.com>
2022-09-12 08:37:18 -0300

    Update DataFormatterTests.cs

--- DataFormatterTests.cs
+++ DataFormatterTests.cs
@@ -11,15 +11,15 @@ public class DateFormatterTests
     public void TestCorrectFormatDate()
     {
         string actualDate = "10/11/1997";
-        string expectedDate = "1997-11.10";
+        string expectedDate = "1997-11-10";
         string finalDate = DateFormatter.ChangeFormat(actualDate);
         Assert.AreEqual(finalDate, expectedDate);
     }
     [Test]
     public void TestIncorrectFormatDate()
     {
-        string wDate = "10/11/1997";
-        string expectedDate = "1997-11.10";
+        string wDate = "0/1/19";
+        string expectedDate = "1997-11-10";
         string convertedDate = DateFormatter.ChangeFormat(wDate);
         Assert.IsFalse(convertedDate.Equals(expectedDate));
     }
